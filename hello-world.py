76e4cde540c2d6081f5a5ffedcf438c8d2bb39de
NathaniellH <nheruy@ucsc.edu>
2024-10-01 11:03:23 -0700

    Added a hello world program
    The repository is now less boring!

---
+++ hello-world.py
@@ -0,0 +1,19 @@
+# This program will 
+# write Hello World!
+
+# This function tells the
+# computer to print
+# Hello Wold!
+def PrintHelloWold():
+	print("Hello World!")
+
+# This defines our main()
+# funtion for ou program
+def main():
+	PrintHelloWorld()
+
+# When we run the program
+# this executes first
+if __name__=="__main__":
+	main()
+	
