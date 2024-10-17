e852e2ac6d8640919f0a10aabfd9f6e35af92f7f
NathaniellH <nheruy@ucsc.edu>
2024-10-16 18:41:48 -0700

    Add files via upload
    Session 2 Prompt for Coding Journal #1

---
+++ CJ2.py
@@ -0,0 +1,20 @@
+#This is for the sum and data type of the sum (floats)
+float_1 = 5.5
+float_2 = 4.4
+Sum_result = float_1 + float_2
+print("Sum:", Sum_result)
+print("Data type of sum:", type(Sum_result))
+
+#This is for the difference and data type of the difference (integers)
+int_1 = 5
+int_2 = 4
+Difference_result = int_1 - int_2
+print("Difference:", Difference_result)
+print("Data type of difference:", type(Difference_result))
+
+#This is for the product and data type of the product (1 float 1 integer)
+float_3 = 5.5
+int_3 = 4
+Product_result = float_3 * int_3
+print("Prodcut:", Product_result)
+print("Data type of product:", type(Product_result))
