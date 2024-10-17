f9ee962680fe778a5b4430ba911cfc0006f8ffb6
NathaniellH <nheruy@ucsc.edu>
2024-10-16 18:07:16 -0700

    Add files via upload
    Session 1 prompt for Coding Journal #1

---
+++ CondingJournal1Attempt2.py
@@ -0,0 +1,22 @@
+#This program will write
+#Nathaniel Heruy
+#He/Him
+#My favorite movie is
+#Moana because it reminds
+#Me of my childhood
+#My favorite food is
+#Steak because it's versatile
+
+#I'm going to assign variables to certain letters to make the printing
+#and just writing the program easier
+
+Intro = "Hi, I'm Nathaniel Heruy!"
+PN = "My pronouns are he/him."
+FM = "My favorite movie is Moana because it reminds me of my childhood."
+FF = "My favorite food is steak because it's versatile."
+
+#Now to print
+print(Intro)
+print(PN)
+print(FM)
+print(FF)
