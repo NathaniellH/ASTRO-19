6726ad2ef6eee1d14fe691f75eee7c73356602db
NathaniellH <nheruy@ucsc.edu>
2024-10-16 18:58:39 -0700

    Add files via upload
    Session 3 Prompt for Coding Journal #1

---
+++ CJ3.py
@@ -0,0 +1,13 @@
+#This is for function definition, now f(x) has the equation x**3 + 8 attatched
+def f(x):
+	return x**3 + 8
+
+#Main function of program
+x = 9
+result = f(x)
+print("result of f(x):", result)
+
+#This is for the "if"
+if result > 27:
+	print("YAY!")
+	
